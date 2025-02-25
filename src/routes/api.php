56468b2a6f78dfe55169d72db21bcbbd8f205e17
Konrad <konasgpro@gmail.com>
2025-02-25 19:51:08 +0100

    Feature: fallback 404 endpoint

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::fallback(function (Request $request) {
+    return response()->json([
+        'message' => 'Resource not found',
+        'status' => 404,
+    ], 404);
+});
